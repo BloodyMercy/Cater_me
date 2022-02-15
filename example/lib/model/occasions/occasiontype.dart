aa31b7f49e9b858fa2f0e7d4c84875c4c1fe454e
brahimmakary <makary40@gmail.com>
2022-02-15 14:13:38 +0200

    uhuh

--- example/lib/model/occasions/occasiontype.dart
+++ example/lib/model/occasions/occasiontype.dart
@@ -1,12 +1,13 @@
 class OccassionType {
   int id;
   String name;
-
-  OccassionType({this.id, this.name});
+String image ;
+  OccassionType({this.id, this.name, this.image});
 
   OccassionType.fromJson(Map<String, dynamic> json) {
     id = json['id']??0;
     name = json['title']??"not found";
+    image= json['image']??"notfound";
   }
 
 }
