446078d668d5f0a2431242688841db92780a9ba9
ahmadstyleme <barbershops.766@gmail.com>
2022-03-01 22:01:58 +0200

    bbhb

--- example/lib/Payment/OrderSucc.dart
+++ example/lib/Payment/OrderSucc.dart
@@ -100,7 +100,7 @@ class _AppointmentSuccessState extends State<AppointmentSuccess> {
                 height: MediaQuery.of(context).size.height / 2,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage('images/Samcater.png'),
+                      image: _serpres.serviceId==1? AssetImage('example/images/orderTracking/sammdaberni.png'):AssetImage('example/images/orderTracking/sammcaterme.png'),
                       fit: BoxFit.contain
                   ),
                   borderRadius: BorderRadius.circular(50),
