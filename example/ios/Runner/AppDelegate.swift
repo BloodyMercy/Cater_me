5d3afad97252090ae7b987638ded7f50a7c771eb
brahimmakary <makary40@gmail.com>
2022-03-02 15:08:58 +0200

    ahmad rifai

---
+++ example/ios/Runner/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Flutter
+import GoogleMaps
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+  GMSServices.provideAPIKey("AIzaSyCokaz4JgSMsvLIsMlkR8LNpm05DQZ3jU4")
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+}
+
+
