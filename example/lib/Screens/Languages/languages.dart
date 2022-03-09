3a0c18a69ba2c298f750d1af2feec87621307bc3
nina <naynawakhatib@outlook.com>
2022-03-09 15:22:03 +0200

    Nina

---
+++ example/lib/Screens/Languages/languages.dart
@@ -0,0 +1,106 @@
+import 'package:CaterMe/NavigationBar/navigation_bar.dart';
+import 'package:CaterMe/Providers/user.dart';
+import 'package:CaterMe/colors/colors.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../language/language.dart';
+
+class Languages extends StatefulWidget {
+  const Languages({Key key}) : super(key: key);
+
+  @override
+  _LanguagesState createState() => _LanguagesState();
+}
+
+class _LanguagesState extends State<Languages> {
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: Center(
+                  child: Image(
+                    image: AssetImage('images/logo.png'),
+                    // width: 800,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.12),
+              Center(
+                  child: Text(
+                'Choose Language',
+                style: TextStyle(
+                    fontFamily: 'BerlinSansFB',
+                    fontSize: 25,
+                    color: Theme.of(context).primaryColor),
+              )),
+              Padding(
+                padding: EdgeInsets.only(top: screenHeight * 0.15),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.1,
+                      right: MediaQuery.of(context).size.width * 0.1),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(120, 60),
+                            primary:
+                                Theme.of(context).primaryColor
+                                ,
+                          ),
+                          onPressed: () {
+
+                          },
+                          child: Container(
+                            child: Text(
+                              'English',
+                              style: TextStyle(
+                                  fontFamily: 'BerlinSansFB',
+                                  color:
+                                     Colors.white ),
+                            ),
+                          )),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(120, 60),
+                            primary:
+                                 Theme.of(context).primaryColor
+                                ,
+                          ),
+                          onPressed: () {
+
+                          },
+                          child: Container(
+                            child: Text(
+                              'Arabic',
+                              style: TextStyle(
+                                  fontFamily: 'BerlinSansFB',
+                                  color: Colors.white ),
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
