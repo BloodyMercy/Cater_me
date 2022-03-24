f47c77052ff0f3b104cd6ff7acc3db52d396f19b
nina <naynawakhatib@outlook.com>
2022-03-24 14:21:04 +0200

    Nina

--- example/lib/Screens/review/Second_review.dart
+++ example/lib/Screens/review/Second_review.dart
@@ -3,14 +3,16 @@ import 'package:CaterMe/Screens/review/Fourth_review.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 import '../../Providers/surveyProvder.dart';
 
-class SecondReview extends StatelessWidget {
+class SecondReview extends StatefulWidget {
+  @override
+  State<SecondReview> createState() => _SecondReviewState();
+}
 
+class _SecondReviewState extends State<SecondReview> {
   @override
   Widget build(BuildContext context) {
-
     final survey = Provider.of<SurveyProvider>(context, listen: true);
     var mediaQueryHeight = MediaQuery.of(context).size.height;
     var mediaQueryWidth = MediaQuery.of(context).size.width;
@@ -32,7 +34,7 @@ class SecondReview extends StatelessWidget {
           child: Column(
             children: [
               Padding(
-                padding:  EdgeInsets.only(top: mediaQueryHeight*0.01),
+                padding: EdgeInsets.only(top: mediaQueryHeight * 0.01),
                 child: Text(
                   'What do we need to improve?',
                   style: TextStyle(color: Colors.white, fontSize: 25),
@@ -43,54 +45,88 @@ class SecondReview extends StatelessWidget {
                 style: TextStyle(color: Colors.white, fontSize: 25),
               ),
               Padding(
-                padding:  EdgeInsets.symmetric(horizontal: mediaQueryWidth*0.1, vertical: mediaQueryHeight*0.05),
-                child:Column(children: [
-             Container(
-               width: mediaQueryWidth,
-               height:mediaQueryHeight/1.8 ,
-               child: ListView.builder(
-                 itemCount: survey.listsurvey.length,
-                 itemBuilder: (context, index) {
-                 return     Padding(
-                   padding:  EdgeInsets.only(bottom: mediaQueryHeight*0.01),
-                   child: ElevatedButton(
-                     onPressed: () {
-                       survey.feedbackoptionid.add(survey.listsurvey[index].id);
-                       Navigator.of(context).push(MaterialPageRoute(
-                           builder: (context) => FourthReview()));
-                     },
-                     child: Row(
-                       children: [
-                         Expanded(
-                           child: Text(
-                             survey.listsurvey[index].title,
-                             style: TextStyle(color: Colors.black),
-                           ),
-                         ),
-                       ],
-                     ),
-                     style: ButtonStyle(
-                         shape: MaterialStateProperty.all(
-                             const RoundedRectangleBorder(
-                                 borderRadius:
-                                 BorderRadius.all(Radius.circular(15)))),
-                         padding: MaterialStateProperty.all(
-                             EdgeInsets.symmetric(
-                               vertical: mediaQueryHeight * 0.015,)),
-                         backgroundColor: MaterialStateProperty.all(
-                             Colors.white)),
-                   ),
-                 );
-               },),
-             )
-
-
-
-
-                ],)
-
-              ),
-
+                  padding: EdgeInsets.symmetric(
+                      horizontal: mediaQueryWidth * 0.1,
+                      vertical: mediaQueryHeight * 0.05),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: mediaQueryWidth,
+                        height: mediaQueryHeight / 1.8,
+                        child: ListView.builder(
+                          itemCount: survey.listsurvey.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: EdgeInsets.only(
+                                  bottom: mediaQueryHeight * 0.01),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if(survey.listsurvey[index].isChecked){
+                                      survey.feedbackoptionid
+                                          .remove(survey.listsurvey[index].id);
+                                      survey.listsurvey[index].isChecked=false;
+                                    }
+                                    else{
+                                      survey.feedbackoptionid
+                                          .add(survey.listsurvey[index].id);
+                                      survey.listsurvey[index].isChecked=true;
+                                    }
+                                  });
+                                },
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        survey.listsurvey[index].title,
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all(
+                                        const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(15)))),
+                                    padding: MaterialStateProperty.all(
+                                        EdgeInsets.symmetric(
+                                      vertical: mediaQueryHeight * 0.015,
+                                    )),
+                                    backgroundColor: survey.listsurvey[index].isChecked? Theme.of(context).primaryColor: MaterialStateProperty.all(
+                                        Colors.white)),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.only(top:mediaQueryHeight*0.05),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => FourthReview()));
+                          },
+                          child: Text(
+                            'Next',
+                            style: TextStyle(color: Colors.black,fontSize: 25),
+                          ),
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                  const RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.all(Radius.circular(15)))),
+                              padding: MaterialStateProperty.all(
+                                EdgeInsets.symmetric(
+                                    vertical: (mediaQueryHeight * 0.074) * 0.3,
+                                    horizontal: (mediaQueryWidth * 0.3)),
+                              ),
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.white)),
+                        ),
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
--- example/lib/model/Survey.dart
+++ example/lib/model/Survey.dart
@@ -2,12 +2,14 @@ class surveyModel {
   int id;
   String title="catch";
   String titleAr="catch";
+  bool isChecked=false;
 
   surveyModel({id, title,titleAr});
 
   surveyModel.fromJson(Map<String, dynamic> json ,String lang) {
     id = json['id'];
     title = lang=="ar"?json['titleAr']:json['title'];
+    isChecked=json['isChecked'];
 
   }
 
