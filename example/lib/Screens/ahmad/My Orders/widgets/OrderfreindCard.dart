022d0f47b87ba8f4138c0fbccb81fc34411321b8
brahimmakary <makary40@gmail.com>
2022-04-11 02:52:24 +0300

    ahmad rifai

--- example/lib/Screens/ahmad/My Orders/widgets/OrderfreindCard.dart
+++ example/lib/Screens/ahmad/My Orders/widgets/OrderfreindCard.dart
@@ -167,7 +167,7 @@ class _OrderfreindCardState extends State<OrderfreindCard> {
   Future<void> share(String link) async {
     await FlutterShare.share(
         title: 'cater me',
-        text: 'Share bill',
+        text: 'Share bill with ${widget.a.name}',
         linkUrl: link,
         chooserTitle: 'Share bill');
   }
